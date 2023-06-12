a57fc57af55ea0dffd517cdd98c233ce18e4746f
NdulueMarvellous <chinedundulue156@gmail.com>
2023-06-12 09:42:01 -1200

    first commit

---
+++ JS/script.js
@@ -0,0 +1,7 @@
+// Typing Animation
+var typed = new Typed(".typing", {
+	strings:["","Software Engineer", "Web Developer/Web Designer", "UI/UX Graphics Designer"],
+	typeSpeed:100,
+	BackSpeed:60,
+	loop:true
+})
